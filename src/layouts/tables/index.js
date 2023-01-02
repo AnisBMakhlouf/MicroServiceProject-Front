b46851a925efc5ec91303cea19e34b33fc42a8cf
Anis Benmakhlouf <anis.benmakhlouf@progedsolutions.com>
2023-01-02 20:57:09 +0100

    Users tabs switch

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -11,19 +11,70 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
-
+import Icon from "@mui/material/Icon";
+import breakpoints from "assets/theme/base/breakpoints";
 // Data
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
-
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 function Tables() {
   const { columns, rows } = authorsTableData();
   const { columns: pColumns, rows: pRows } = projectsTableData();
-  
+  const [tabsOrientation, setTabsOrientation] = useState("horizontal");
+  const [tabValue, setTabValue] = useState(0);
+
+  useEffect(() => {
+    // A function that sets the orientation state of the tabs.
+    function handleTabsOrientation() {
+      return window.innerWidth < breakpoints.values.sm
+        ? setTabsOrientation("vertical")
+        : setTabsOrientation("horizontal");
+    }
+
+    /** 
+     The event listener that's calling the handleTabsOrientation function when resizing the window.
+    */
+    window.addEventListener("resize", handleTabsOrientation);
+
+    // Call the handleTabsOrientation function to set the state with the initial value.
+    handleTabsOrientation();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleTabsOrientation);
+  }, [tabsOrientation]);
+
+  const handleSetTabValue = (event, newValue) => setTabValue(newValue);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      <AppBar position="static">
+              <Tabs orientation={tabsOrientation} value={tabValue} onChange={handleSetTabValue} onclick={console.log(tabValue)}>
+                <Tab
+                  label="Admins"
+                  icon={
+                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
+                    </Icon>
+                  }
+                />
+                <Tab
+                  label="Enseignants"
+                  icon={
+                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
+                    </Icon>
+                  }
+                />
+                <Tab
+                  label="Agents de Tirage"
+                  icon={
+                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
+                    </Icon>
+                  }
+                />
+              </Tabs>
+            </AppBar>
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
@@ -39,50 +90,41 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                  All users
+                {(() => {
+                  switch (tabValue) {
+                    case 0:
+                      return 'Admins'
+                    case 1:
+                      return 'Enseignants'
+                    case 2:
+                      return 'Agents de Tirage'
+                  }
+                })()}
                 </MDTypography>
               </MDBox>
               <MDBox pt={3}>
-                <DataTable
-                  table={{ columns, rows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                  Projects Table
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
+              {(() => {
+                  switch (tabValue) {
+                    case 0:
+                      return <DataTable
+                      table={{ columns, rows }}
+                      isSorted={false}
+                      entriesPerPage={false}
+                      showTotalEntries={false}
+                      noEndBorder
+                    />
+                    case 1:
+                      return ''
+                    case 2:
+                      return ''
+                  }
+                })()}
+                
               </MDBox>
             </Card>
           </Grid>
         </Grid>
       </MDBox>
-      <Footer />
     </DashboardLayout>
   );
 }
